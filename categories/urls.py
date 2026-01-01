fa6082e8729f79f7c04f20d3c63b18e909057571
r-on-s <ronyarif700@gmail.com>
2026-01-01 20:36:08 +0600

    creating category and post

--- categories/urls.py
+++ categories/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-   #path('add',views.add_category,name='add_category'),
+   path('add/',views.add_category,name='add_category'),
 ]
