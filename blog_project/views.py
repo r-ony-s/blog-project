671a3f7d2f6e7cb362356103535652afdccaa4f8
r-on-s <ronyarif700@gmail.com>
2026-01-02 14:38:07 +0600

    showing post to frontend

---
+++ blog_project/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from posts.models import Post
+# Create your views here.
+def home(request):
+    data = Post.objects.all()
+    return render(request, 'home.html', {'data': data})
